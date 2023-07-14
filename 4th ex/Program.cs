8a22b085b7d63cbe9e4cc4488ff6f2e576fa9c4f
Liz Grig <a-white-wolf2000@ya.ru>
2023-07-15 00:59:34 +0300

    math

---
+++ 4th ex/Program.cs
@@ -0,0 +1,11 @@
+﻿//Принимает целое число а, выдаёт все целые между -а и а.
+
+Console.Write("Insert number: ");
+int Number = int.Parse(Console.ReadLine());
+int count = 0 - Number;
+
+while(count <= Number)
+{
+    Console.Write($"{count} ");
+    count++;
+}
---
+++ 5th ex/Program.cs
@@ -0,0 +1,14 @@
+﻿// Принимает трёхзначное, выдаёт остаток от деления на 10.
+
+Console.Write("Insert number: ");
+int a = int.Parse(Console.ReadLine()), result = 0;
+
+if (a > 99 && a <1000)
+{
+    result = a%10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("wrong number");
+}
