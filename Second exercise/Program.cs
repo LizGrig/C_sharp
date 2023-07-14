217a3d6f7020593e371f9081a1dd709d319f244e
Liz Grig <a-white-wolf2000@ya.ru>
2023-07-15 00:15:52 +0300

    new exercises

---
+++ Second exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+1. Ввод 2х чисел и проверка, является ли первое квадратом второго.
+*/
+
+Console.Write("Insert first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+bool square = false;
+
+if (a == b*b)
+{
+    square = true;
+}
+
+Console.WriteLine($"It's {square} that {a} is a square of {b}");
---
+++ Third exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+2. Вывод название дня недели по введённому номеру.
+*/
+
+Console.Write ("Insert number: ");
+int NumberOfDay = Convert.ToInt32(Console.ReadLine());
+
+Dictionary <int,string> map = new Dictionary<int, string>()
+{
+    {1, "Mon"},
+    {2, "Tue"},
+    {3, "Wed"},
+    {4, "Thur"},
+    {5, "Fri"},
+    {6, "Sat"},
+    {7, "Sun"}
+};
+
+if (NumberOfDay > 0 && NumberOfDay < 8)
+{
+    Console.WriteLine(map[NumberOfDay]);
+}
+else
+{
+    Console.WriteLine("Wrong number");
+}
