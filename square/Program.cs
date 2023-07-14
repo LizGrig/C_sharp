c9d0f4865a1a15b668262227ba816034090a705e
Liz Grig <a-white-wolf2000@ya.ru>
2023-07-14 23:24:22 +0300

    new exercise square of number and input of numbers

---
+++ square/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Ввод числа и вывод его квадрата.
+В C# можно вводить только строки, поэтому для ввода чисел используется конвертация.
+
+Вариантов несколько:
+1. int <name> = 0;
+   int.TryParse(Console.ReadLine(), out <name>>);
+
+2. <name> = int.Parse(Console.ReadLine());
+
+3. <name> = Convert.ToInt32(Console.ReadLine());
+*/
+
+Console.Write("Введите число: ");
+int a = 0;
+int.TryParse(Console.ReadLine(), out a);
+Console.WriteLine(a);
+
+int power = 2; // power of a
+int result = (int)Math.Pow(a,power);
+/*
+Метод Math работает с типом double. Чтобы не менять тип, его нужно конвертировать.
+Либо через Convert.ToInt32(Math.<..>), либо через (int).
+*/
+Console. WriteLine($"Square of {a} is {result}");
